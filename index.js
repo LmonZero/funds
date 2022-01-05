011f54311604f6816440542915c3c7bb7cbdd0b1
zero <linxiqinnolemon@163.com>
2022-01-05 13:22:00 +0800

    aa

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const tools = require('./lib/tools')
 const appHandeler = require('./appHandler/appHandler')
 // const mysqlHelper = require('./lib/helper/helper_mysql')
 
-
 function main() {
     // const mysql = new mysqlHelper()
     let appHandelerInstance = new appHandeler()
